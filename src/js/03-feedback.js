47fa03d0fbd78a258e6ba7d4a1a99b3ed7d1bcb7
Oleksii Borshch <sergeevi44@gmail.com>
2022-04-30 02:03:01 +0300

    task3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,30 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
 };
-const email = refs.form.elements.email.value;
-const message = refs.form.message.value;
 
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
+
+if (localStorage.getItem('feedback-form-state')) {
+  const emailData = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  const messageData = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+  refs.form.elements.email.value = emailData;
+  refs.form.elements.message.value = messageData;
+}
+
+function onFormInput(evt) {
+  const email = refs.form.elements.email.value;
+  const message = refs.form.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  const dataForStorage = localStorage.setItem(
-    'feedback-form-state',
-    JSON.stringify({ email, message }),
-  );
+  const email = refs.form.elements.email.value;
+  const message = refs.form.message.value;
+  console.log({ email, message });
+  localStorage.removeItem('feedback-form-state');
+  refs.form.reset();
 }
